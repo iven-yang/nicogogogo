b012adddcedcb275a204e6a8c344ce13870679fa
iven-yang <risingsky1@gmail.com>
2017-10-05 21:27:09 -0400

    gogogo

--- Back End/server.go
+++ Back End/server.go
@@ -1,1 +1,37 @@
-{\rtf1}
+package main
+
+import (
+    "fmt"
+    "html"
+    "log"
+    "net/http"
+)
+
+func login(w http.ResponseWriter, r *http.Request) {
+    fmt.Println("method:", r.Method) //get request method
+    if r.Method == "GET" {
+        t, _ := template.ParseFiles("login.gtpl")
+        t.Execute(w, nil)
+    } else {
+        r.ParseForm()
+        // logic part of log in
+        fmt.Println("username:", r.Form["Username"])
+        fmt.Println("password:", r.Form["Password"])
+    }
+}
+
+func main() {
+
+    http.HandleFunc("/",
+		func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+		})
+    
+    http.HandleFunc("/login",
+		func(w http.ResponseWriter, r *http.Request){
+			fmt.Fprintf(w, "Hi")
+		})
+
+    log.Fatal(http.ListenAndServe(":8081", nil))
+
+}
