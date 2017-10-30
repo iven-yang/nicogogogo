c2cc8a3418f9cf15128a973bc2d831148bd23e1f
Matt Foley <mfoley@opendns.com>
2017-10-30 00:26:26 -0400

    Added full request example

--- test.go
+++ test.go
@@ -6,5 +6,13 @@ import(
 )
 
 func main() {
+    testRequest := common.Request{SessionID: "asdf",
+                                  Action: common.LOGOUT,
+                                  Data: map[string]string{},
+                                 }
+    testRequest.Data["asdf"] = "test"
     fmt.Println(common.LOGIN)
+    fmt.Println(testRequest.SessionID)
+    fmt.Println(testRequest.Action)
+    fmt.Println(testRequest.Data["asdf"])
 }
