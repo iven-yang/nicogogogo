385e476e722691ffbb41005978e2317fbf7eca92
iven-yang <risingsky1@gmail.com>
2017-10-30 23:10:38 -0400

    some backend funcs

--- backend.go
+++ backend.go
@@ -1,12 +1,31 @@
 package main
 
 import(
-    "time"
+	"encoding/json"
+    "io/ioutil"
+	"time"
     "net"
     "fmt"
-    "niconicogo/common"
-    "encoding/gob"
+    "./common"
+    "os"
+	"path"
+	"encoding/gob"
 )
+
+type Post struct {
+    Content string
+	Timestr string
+    Time time.Time
+}
+
+type User struct {
+    Username string
+    Hash []byte
+    SessionID string
+    Created time.Time
+    Follows []string
+    Posts []*Post
+}
 
 func handleConnection(conn net.Conn) {
     request := common.Request{}
@@ -21,9 +40,11 @@ func handleConnection(conn net.Conn) {
         case common.LOGOUT:
             fmt.Println("Handling logout action")
         case common.REGISTER:
-            fmt.Println("Handling register action")
+			fmt.Println("Handling register action")
+            // db_register(db_JSON_to_user())
         case common.DELETE:
             fmt.Println("Handling delete action")
+			// db_delete_user()
         case common.FOLLOW:
             fmt.Println("Handling follow action")
         case common.POST:
@@ -32,9 +53,59 @@ func handleConnection(conn net.Conn) {
             fmt.Println("Handling feed action")
         case common.PROFILE:
             fmt.Println("Handling profile action")
-        case default:
-            fmt.Println("Unrecognized action")
+        // case default:
+            // fmt.Println("Unrecognized action")
     }
+}
+
+func db_update_user(username string, sessionid string, follows []string, post Post){
+	
+}
+
+func db_register(user User) {
+	fmt.Println("JSON DATA:")
+	newUserBytes := db_user_to_JSON(user)
+	fmt.Println(string(newUserBytes)[:])
+	writeerr := ioutil.WriteFile(path.Join("db/users", user.Username+".json"), newUserBytes, 0644)
+	if writeerr != nil {
+		panic(writeerr)
+	}
+}
+
+func db_delete_user(username string) {
+	err := os.Remove(path.Join("db/users", username+".json"))
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+	fmt.Println("User Removed: ", username)
+}
+
+func db_check_user_exists(username string) bool {
+	if _, err := os.Stat(path.Join("db/users", username+".json")); !os.IsNotExist(err) {
+		return true
+	}
+	return false
+}
+
+// converting user struct to JSON string
+func db_user_to_JSON(user User) []byte {
+	JSON_string, _ := json.MarshalIndent(user, "", "    ")
+	return JSON_string
+}
+
+// converting JSON string to user struct
+func db_JSON_to_user(username string) User {
+	dat, err := ioutil.ReadFile(path.Join("db/users", username+".json"))
+	if err != nil {
+		panic(err.Error())
+	}
+	
+	var user User
+	if err := json.Unmarshal(dat, &user); err != nil {
+		panic(err)
+	}
+	return user
 }
 
 func mainLoop() {
